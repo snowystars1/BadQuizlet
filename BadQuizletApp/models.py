72bcf7284184c3727d05a63de67332ce3ddaea57
Megan <megseees@gmail.com>
2019-04-15 15:16:18 -0400

    .gitignore file

--- BadQuizletApp/models.py
+++ BadQuizletApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Cards(models.Model):
+	set_id = models.IntegerField(default=0)
+	set_name = models.CharField(max_length=100)
+	term = models.CharField(max_length = 200)
+	definition = models.CharField(max_length = 400)
