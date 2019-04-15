7ad70d22a122d35323ca46ff059383019e97d98b
Eimti <31927511+Eimti@users.noreply.github.com>
2019-04-15 16:00:30 -0400

    Basic html files created

--- BadQuizletApp/views.py
+++ BadQuizletApp/views.py
@@ -5,4 +5,16 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	return render(request, "homepage.html");
+	return render(request, "homepage.html")
+
+def createSet(request):
+	return render(request, "create.html")
+
+def viewAll(request):
+	return render(request, "viewAll.html")
+
+def random(request):
+	return render(request, "random.html")
+
+def viewCard(request):
+	return render(request, "viewCard.html")
