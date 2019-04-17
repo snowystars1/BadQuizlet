d304b860255978e4cf3fc8f91bf03ac1238fed7d
Megan <megseees@gmail.com>
2019-04-17 15:35:37 -0400

    add Card function

--- BadQuizletApp/views.py
+++ BadQuizletApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 # from django.template.loader import get_template
 from django.http import HttpResponse
 
+from .models import Cards
 # Create your views here.
 
 def index(request):
@@ -18,3 +19,11 @@ def random(request):
 
 def viewCard(request):
 	return render(request, "viewCard.html")
+
+def createSet(request):
+	return render(request, "create.html")
+
+
+def addCard(setName, termName, definitionName):
+	b = Cards.objects.create(set_name=setName, term = termName, definition=definitionName)
+	b.save()
