0a0aa2ed9215d844d5b327fda7e7c603aa8bdaba
Eimti <31927511+Eimti@users.noreply.github.com>
2019-04-17 15:36:25 -0400

    Initial forms stuff

---
+++ BadQuizletApp/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+
+class CreateCardForm(forms.Form):
+	setName = forms.CharField(label='Set Name', max_length=100)
+
+	card1front = forms.CharField(label='Card 1 Front', max_length = 100)
+	card1back = forms.CharField(label='Card 1 Back', max_length = 100)
+	card2front = forms.CharField(label='Card 2 Front', max_length = 100)
+	card2back = forms.CharField(label='Card 2 Back', max_length = 100)
+	card3front = forms.CharField(label='Card 3 Front', max_length = 100)
+	card3back = forms.CharField(label='Card 3 Back', max_length = 100)
+	card4front = forms.CharField(label='Card 4 Front', max_length = 100)
+	card4back = forms.CharField(label='Card 4 Back', max_length = 100)
+	card5front = forms.CharField(label='Card 5 Front', max_length = 100)
+	card5back = forms.CharField(label='Card 5 Back', max_length = 100)
+	card6front = forms.CharField(label='Card 6 Front', max_length = 100)
+	card6back = forms.CharField(label='Card 6 Back', max_length = 100)
+	card7front = forms.CharField(label='Card 7 Front', max_length = 100)
+	card7back = forms.CharField(label='Card 7 Back', max_length = 100)
+	card8front = forms.CharField(label='Card 8 Front', max_length = 100)
+	card8back = forms.CharField(label='Card 8 Back', max_length = 100)
+	card9front = forms.CharField(label='Card 9 Front', max_length = 100)
+	card9back = forms.CharField(label='Card 9 Back', max_length = 100)
+	card10front = forms.CharField(label='Card 10 Front', max_length = 100)
+	card10back = forms.CharField(label='Card 10 Back', max_length = 100)
+	
